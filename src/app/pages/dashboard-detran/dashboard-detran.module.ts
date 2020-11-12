7ab888df42e319fd9f000050de5f97e849ba726a
Mardoniosc <mardonio@live.com>
2020-11-12 17:48:42 -0300

    Criado o componente home

--- src/app/pages/dashboard-detran/dashboard-detran.module.ts
+++ src/app/pages/dashboard-detran/dashboard-detran.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardDetranRoutingModule } from './dashboard-detran-routing.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     DashboardDetranRoutingModule
